535d5ab90d8003ca123a2418fcea9ec126bba2f5
admin <>
2018-07-19 11:45:47 +0700

    {/src/main/java/com/myspace/leaveapproval/LeaveData.java}

--- src/main/java/com/myspace/leaveapproval/LeaveData.java
+++ src/main/java/com/myspace/leaveapproval/LeaveData.java
@@ -7,29 +7,63 @@ package com.myspace.leaveapproval;
 @javax.persistence.Entity
 public class LeaveData implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(generator = "LEAVEDATA_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "LEAVEDATA_ID_GENERATOR", sequenceName = "LEAVEDATA_ID_SEQ")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(generator = "LEAVEDATA_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(name = "LEAVEDATA_ID_GENERATOR", sequenceName = "LEAVEDATA_ID_SEQ")
+	private java.lang.Long id;
 
-    public LeaveData() {
-    }
-    
-    public LeaveData(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Duration")
+	private java.lang.Double duration;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Nama Pegawai")
+	private java.lang.String employeName;
 
+	@org.kie.api.definition.type.Label(value = "Tanggal start cuti")
+	private java.lang.String startDate;
 
+	public LeaveData() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public java.lang.Double getDuration() {
+		return this.duration;
+	}
+
+	public void setDuration(java.lang.Double duration) {
+		this.duration = duration;
+	}
+
+	public java.lang.String getEmployeName() {
+		return this.employeName;
+	}
+
+	public void setEmployeName(java.lang.String employeName) {
+		this.employeName = employeName;
+	}
+
+	public java.lang.String getStartDate() {
+		return this.startDate;
+	}
+
+	public void setStartDate(java.lang.String startDate) {
+		this.startDate = startDate;
+	}
+
+	public LeaveData(java.lang.Long id, java.lang.Double duration,
+			java.lang.String employeName, java.lang.String startDate) {
+		this.id = id;
+		this.duration = duration;
+		this.employeName = employeName;
+		this.startDate = startDate;
+	}
 
 }
